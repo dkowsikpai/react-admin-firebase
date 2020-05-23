6cfba0b73194632201ab6e9b69f7083719ff97f8
Kowsik Nandagopan D <dkowsikpai@protonmail.com>
2020-05-23 23:19:20 +0530

    Update firebase.config.js

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,10 +1,3 @@
 export default {
-    apiKey: "AIzaSyDWYtjT3iBUse6rDfcnJS3CxhijsyaOrzY",
-    authDomain: "bispages.firebaseapp.com",
-    databaseURL: "https://bispages.firebaseio.com",
-    projectId: "bispages",
-    storageBucket: "bispages.appspot.com",
-    messagingSenderId: "150946366403",
-    appId: "1:150946366403:web:b6fe16b3f3b48cac3e3376",
-    measurementId: "G-8NXBCEDRSL"
+
 };
